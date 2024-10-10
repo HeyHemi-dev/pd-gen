291a674b3d0184659047c428aafd023c2753d4c1
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-10-10 16:24:10 +1300

    Make shadcn work

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,10 +1,14 @@
 import type { MetaFunction } from '@remix-run/node'
-import { AppSettings } from '~/data'
+//import { AppSettings } from '~/data'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Admin' }, { name: 'description', content: 'Admin panel' }]
 }
 
 export default function Admin() {
-  return <div>Admin Panel</div>
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+    </div>
+  )
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 export default {
   plugins: {
+    'postcss-preset-env': {
+      stage: 0, // Enables all modern CSS features
+      features: {
+        'color-functional-notation': true, // Enables modern color syntax
+        'custom-properties': true,
+        'nesting-rules': true,
+      },
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
